8403d1dc3bb313c7fc73df5bb4c0a43f5600f685
Allen Snow <allen@allensnow.com>
2024-06-05 07:22:37 -0700

    Initial nextjs app

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // basePath: "/allensnow-com-web",
+    // assetPrefix: "/allensnow-com-web/",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    images: {
+      unoptimized: true
+    }
+  };
+  
+  export default nextConfig;
